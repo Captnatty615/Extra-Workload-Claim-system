158c61d39177ea169cf14ab0330adb1f6f40e98d
Captnatty615 <abdulkarimnatty615@gmail.com>
2024-01-02 14:30:18 +0300

    implemnting the components as child parent

---
+++ react/src/views/ClaimForm.jsx
@@ -0,0 +1,41 @@
+import  { useState } from 'react';
+import Personal from './Personal';
+import Claim from './Claim';
+import axios from 'axios';
+
+
+export default function ClaimForm() {
+   
+  const [personalData, setPersonalData] = useState({});
+  const [claimData, setClaimData] = useState({});
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const handlePersonalSubmit = (data) => {
+    setPersonalData(data);
+    setCurrentStep(2);
+  };
+
+  const handleClaimSubmit = (data) => {
+    setClaimData(data);
+    submitFormData();
+  };
+
+  const submitFormData = async () => {
+    try {
+      await axios.post('/api/claims', {
+        personalData,
+        claimData,
+      });
+      console.log('data added!');
+    } catch (error) {
+      console.error('error adding data');
+    }
+  };
+  
+    return (
+        <>
+        {currentStep === 1 && <Personal onSubmit={handlePersonalSubmit} />}
+        {currentStep === 2 && <Claim onSubmit={handleClaimSubmit} />}
+        </>
+    )
+}
